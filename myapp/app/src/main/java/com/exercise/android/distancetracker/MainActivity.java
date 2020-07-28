7b73d3f4e0f92a4185b72c81c280d9009e515441
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-28 14:08:00 +0530

    Step counter

--- myapp/app/src/main/java/com/exercise/android/distancetracker/MainActivity.java
+++ myapp/app/src/main/java/com/exercise/android/distancetracker/MainActivity.java
@@ -1,11 +1,13 @@
 package com.exercise.android.distancetracker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -21,12 +23,13 @@ public class MainActivity extends AppCompatActivity {
     SensorEventListener sel = new SensorEventListener(){
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
         public void onSensorChanged(SensorEvent event) {
-            float[] values = event.values;
-            textView1.setText("x: "+values[0]+"\ny: "+values[1]+"\nz: "+values[2]);
+
+            textView1.setText(String.valueOf(event.values[0]));
 
 
         }
     };
+
 
 
 
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView1=(TextView)findViewById(R.id.textview);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         mSensorManager.registerListener(sel, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
