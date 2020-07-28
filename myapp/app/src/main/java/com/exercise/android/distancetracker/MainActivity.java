84ea8a4dc6d8635bbeea074240c36b4f7c4ba522
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-28 11:54:50 +0530

    Sensoreventlistener

--- myapp/app/src/main/java/com/exercise/android/distancetracker/MainActivity.java
+++ myapp/app/src/main/java/com/exercise/android/distancetracker/MainActivity.java
@@ -2,13 +2,33 @@ package com.exercise.android.distancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    SensorManager sm = null;
+    float[] values;
+    List list;
 
+    SensorEventListener sel = new SensorEventListener(){
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        public void onSensorChanged(SensorEvent event) {
+            values = event.values;
+            Toast.makeText(MainActivity.this,"0"+values[0],Toast.LENGTH_SHORT).show();
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       
     }
 }
