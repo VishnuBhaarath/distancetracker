ce5a02809d5a078daf6933a85ea2d8ceca81253a
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-29 21:52:38 +0530

    Adding

--- distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
+++ distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
@@ -18,44 +18,42 @@ import android.widget.Toast;
 import android.location.Location;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     TextView textview;
-    FusedLocationProviderClient fusedLocationClient;
+
     private int locationRequestCode = 1000;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview = (TextView) findViewById(R.id.textview1);
-
-
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-            textview.setText("VB");
-            fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener <Location>() {
-                @Override
-                public void onSuccess(Location location) {
-
-                    if(location!=null){
-                        Toast.makeText(MainActivity.this," "+location.getLatitude(),Toast.LENGTH_SHORT).show();
-
-
-                    }
-                    else{
-                        Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-
-
-
-
-
-
-
+        requestlocationupdates();
     }
+    public void requestlocationupdates() {
+        fusedLocationProviderClient = new FusedLocationProviderClient(this);
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setInterval(4000);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+               double latitude=locationResult.getLastLocation().getLatitude();
+                double  longitude=locationResult.getLastLocation().getLongitude();
+                // Log.e("Mainactivity", "lat" + locationResult.getLastLocation().getLatitude());
+                //Log.e("Mainactivity", "Long" + locationResult.getLastLocation().getLongitude());
+            }
+        }, getMainLooper());
+    }
+
 }
