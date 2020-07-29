54d9a658c09a2603ca5e5ed944e0bf251db22225
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-29 14:04:49 +0530

    Add

--- distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
+++ distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
@@ -2,7 +2,7 @@ package com.exercise.android.distanceusinggps;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
+import android.location.LocationProvider;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -14,55 +14,42 @@ import android.location.LocationManager;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.location.Location;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     TextView textview;
+    FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview = (TextView) findViewById(R.id.textview1);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener <Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!=null){
+                    Toast.makeText(MainActivity.this," "+location.getLatitude(),Toast.LENGTH_SHORT).show();
 
-        textview.setText("bujen");
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-
-        if (lm != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+                    textview.setText("Latitude"+location.getLatitude());
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                }
             }
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double x = location.getLatitude();
-                    double y = location.getLongitude();
-                    textview.setText("dfdfdg");
+        });
 
-                }
 
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
 
-                }
 
-                @Override
-                public void onProviderEnabled(String s) {
 
-                }
 
-                @Override
-                public void onProviderDisabled(String s) {
 
-                }
-            });
-        }
     }
 }
