d014c04f43e4e81f04cd17d65e1abefe9653da54
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-07-29 18:53:57 +0530

    Add

--- distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
+++ distanceusinggps/app/src/main/java/com/exercise/android/distanceusinggps/MainActivity.java
@@ -31,41 +31,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview = (TextView) findViewById(R.id.textview1);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // reuqest for permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    locationRequestCode);
-
-        } else {
 
 
-        }
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            switch (requestCode) {
-                case 1000: {
-                    // If request is cancelled, the result arrays are empty.
-                    if (grantResults.length > 0
-                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+            textview.setText("VB");
+            fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener <Location>() {
+                @Override
+                public void onSuccess(Location location) {
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-        textview.setText("VB");
-        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener <Location>() {
-            @Override
-            public void onSuccess(Location location) {
-
-                if(location!=null){
-                    Toast.makeText(MainActivity.this," "+location.getLatitude(),Toast.LENGTH_SHORT).show();
+                    if(location!=null){
+                        Toast.makeText(MainActivity.this," "+location.getLatitude(),Toast.LENGTH_SHORT).show();
 
 
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else{
-                    Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+            });
+
+
+
+
 
 
 
